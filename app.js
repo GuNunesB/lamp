b6156cb6140d221d648f85bb421756d6b45666ef
GuNunesB <gununes280@gmail.com>
2024-11-07 20:42:00 -0300

    quebrar lâmpada

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
  * Simulador de lâmpada
  * @author Gustavo Nunes Bispo
  */
+
+function quebrar() {
+    document.getElementById('lamp_off').src="img/broken.jpg"
+    // Reprodizando um arquivo de áudio no JS
+    // Classe de áudio:
+    let som = new Audio() 
+    som.src = "sound/glassbreaking.wav" 
+    som.play()
+}
