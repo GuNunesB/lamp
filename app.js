dec3ff90cb8a217b4287f92eeb5e2f4e28cacf41
GuNunesB <gununes280@gmail.com>
2024-11-07 22:16:10 -0300

    Adicionando funcionalidades

--- app.js
+++ app.js
@@ -3,11 +3,31 @@
  * @author Gustavo Nunes Bispo
  */
 
-function quebrar() {
-    document.getElementById('lamp_off').src="img/broken.jpg"
-    // Reprodizando um arquivo de áudio no JS
+let chave = false // O interrupitor está desligado
+let lampada = true // A Lâmpada está ok
+
+function quebrar() {// Reprodizindo um arquivo de áudio no JS
+    if (lampada == true) {
     // Classe de áudio:
     let som = new Audio() 
     som.src = "sound/glassbreaking.wav" 
     som.play()
+    
+    document.getElementById('lamp').src="img/broken.jpg"
+    lampada = false 
+    } else {
+
+    }
+}
+
+function onoff() {
+    if (chave === false && lampada === true) {
+        document.getElementById('Interruptor').src="img/swon.png"
+        chave = true
+        document.getElementById('lamp').src="img/on.jpg"
+    } else if (lampada === true){
+        document.getElementById('Interruptor').src="img/swoff.png"
+        chave = false
+        document.getElementById('lamp').src="img/off.jpg"
+    } 
 }
